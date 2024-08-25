00a1323a025f21ce6fa5b1b85b2780960b7db4e2
Nima Saadati <nimodbdev@gmail.com>
2024-08-25 21:12:50 +0400

    Add user input handling to calculate nth Fibonacci number

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::io;
 fn main() {
-    println!("Hello World!")
+    let n = get_input();
+    let result = fibonacci(n);
+    println!("The {n}th Fibonacci number is: {result}");
+}
+
+fn get_input() -> u32 {
+    println!("Enter the value of n:");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let n: u32 = input.trim().parse().expect("Please enter a valid number");
+    n
 }
 
 fn fibonacci(n: u32) -> u32 {
