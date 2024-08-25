a4c3a893747ac0f55046f7d7c40a9137cbd0e667
Nima Saadati <nimodbdev@gmail.com>
2024-08-25 21:27:45 +0400

    Add basic error handling for non-numeric or invalid user input

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 use std::io;
 fn main() {
-    let n = get_input();
-    let result = fibonacci(n);
-    println!("The {n}th Fibonacci number is: {result}");
-}
-
-fn get_input() -> u32 {
     println!("Enter the value of n:");
 
     let mut input = String::new();
@@ -13,8 +7,15 @@ fn get_input() -> u32 {
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    let n: u32 = input.trim().parse().expect("Please enter a valid number");
-    n
+    let n: u32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a valid positive integer.");
+            return;
+        }
+    };
+    let result = fibonacci(n);
+    println!("The {n}th Fibonacci number is: {result}");
 }
 
 fn fibonacci(n: u32) -> u32 {
